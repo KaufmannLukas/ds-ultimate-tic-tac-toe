7fad2f45a5853c00bc212dccbd70974c8a842638
Nils Schillmann <nils.schillmann@pexon-consulting.de>
2023-11-06 15:37:22 +0100

    change color names in game, fix current player bug

--- environments/game.py
+++ environments/game.py
@@ -25,8 +25,8 @@ class Game:
 
 
     def __init__(self):
-        self.white = Game._Player("white") # white is O and starts
-        self.black = Game._Player("black") # black is X
+        self.white = Game._Player("white (X)") 
+        self.black = Game._Player("black (O)") 
         self.winner = None
         self.done = False
     
@@ -104,7 +104,7 @@ class Game:
             #print(f"{current_player.color} wins local game {game_idx}")
             if Game.check_win(current_player.wins):
                 win_global_game = True
-                self.winner = self.current_player.color
+                self.winner = current_player.color
                 #print(f"{current_player.color} wins global game")
                 self.done = True
         
