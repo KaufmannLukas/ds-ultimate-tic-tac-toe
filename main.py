884266c8f0c8ca03cf25af1a4e21ec379e738b4d
Nils Schillmann <nils.schillmann@pexon-consulting.de>
2023-11-06 11:37:22 +0100

    implement n game runs.

--- main.py
+++ main.py
@@ -1,31 +1,76 @@
 
 from gym_envs.uttt_env import UltimateTicTacToeEnv, random_policy
+import pandas as pd
+import numpy as np
+
+import time
+
+
+def combine_moves(white_moves, black_moves):
+    # Interleave the moves, assuming white starts first
+    moves = [None]*81  # Initialize a list with 81 moves
+    moves[:len(white_moves)*2:2] = white_moves
+    moves[1:len(black_moves)*2:2] = black_moves
+    return moves
+
+
 
 if __name__ == "__main__":
     
     env = UltimateTicTacToeEnv()
-    state = env.reset()
 
     reward_history_white = []
     reward_history_black = []
-
-    done = False
     counter = 0
-    while not done:
-        action = random_policy(state)
-        state, reward, done, _ = env.step(action)
 
-        if counter % 2:
-            reward_history_black.append(reward)
-        else:
-            reward_history_white.append(reward)
+    move_history = pd.DataFrame(columns=[i for i in range(81)])
+    games_data = []
 
-        env.render()
+    start_time = time.time()
+    print("start: ", start_time)
 
-        print("")
-        print("-"*31)
-        print("")
-        counter = counter + 1
-    print(f"white rewards: {reward_history_white}", sum(reward_history_white))
-    print(f"black rewards: {reward_history_black}", sum(reward_history_black))
+
+    for i in range(1000):
+
+        done = False
+        state = env.reset()
+
+        while not done:
+            action = random_policy(state)
+            state, reward, done, _ = env.step(action)
+
+            if counter % 2:
+                reward_history_black.append(reward)
+            else:
+                reward_history_white.append(reward)
+
+            # env.render()
+
+            # print("")
+            # print("-"*31)
+            # print("")
+            counter = counter + 1
+        
+
+        game_moves = combine_moves(env.game.white.history, env.game.black.history)
+        games_data.append(game_moves)
+        
+    df_games = pd.DataFrame(games_data, columns=[str(i) for i in range(81)])
+    
+
+    end_time = time.time()
+    print("end:", end_time)
+    duration = end_time - start_time
+    print("duration: ", duration)
+
+    df_games.to_csv("history.csv")
+    #move_history.to_csv("history.csv")
+
+
+
+    print("done")
+
+
+        # print(f"white rewards: {reward_history_white}", sum(reward_history_white))
+        # print(f"black rewards: {reward_history_black}", sum(reward_history_black))
     
