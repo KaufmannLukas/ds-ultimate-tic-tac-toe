b3d8f48f5d92daa0023cab9a70beca4ce90003c0
Nils Schillmann <nils.schillmann@pexon-consulting.de>
2023-11-06 09:27:51 +0100

    add reward history to main

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ if __name__ == "__main__":
     env = UltimateTicTacToeEnv()
     state = env.reset()
 
+    reward_history_white = []
+    reward_history_black = []
+
     done = False
+    counter = 0
     while not done:
         action = random_policy(state)
         state, reward, done, _ = env.step(action)
+
+        if counter % 2:
+            reward_history_black.append(reward)
+        else:
+            reward_history_white.append(reward)
+
         env.render()
 
         print("")
         print("-"*31)
         print("")
+        counter = counter + 1
+    print(f"white rewards: {reward_history_white}", sum(reward_history_white))
+    print(f"black rewards: {reward_history_black}", sum(reward_history_black))
+    
