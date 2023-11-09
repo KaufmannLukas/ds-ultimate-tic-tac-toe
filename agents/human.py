72f6f4dbc89c4cf4d0300c4fb114ea02389ec221
Nils Schillmann <nils.schillmann@pexon-consulting.de>
2023-11-09 17:06:58 +0100

    setup human agent

---
+++ agents/human.py
@@ -0,0 +1,29 @@
+from agents.agent import Agent
+from environments.game import Game
+
+
+class Human(Agent):
+
+    def __init__(self):
+        super().__init__()
+
+    def play(self, game: Game) -> tuple:
+        valid_move = False
+        while not valid_move:
+            inpt = input("Enter your move! >> ") # (e.g., '2 3', or '23', or '2,3')
+            nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            move = []
+            counter = 0
+            for c in inpt:
+                if c in nums:
+                    counter += 1
+                    move.append(int(c)-1)
+            if counter < 2:
+                continue
+            move = (move[0], move[1])
+            if game.check_valid_move(*move):
+                break
+            print(f"{move} is not a valid move")
+            
+        return move
+    
