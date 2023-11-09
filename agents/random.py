7844b5e843e9b4e4942d9410e572a767e75b060e
Nils Schillmann <nils.schillmann@pexon-consulting.de>
2023-11-09 17:14:31 +0100

    add random agent

---
+++ agents/random.py
@@ -0,0 +1,13 @@
+from random import sample
+from agents.agent import Agent
+from environments.game import Game
+
+
+class Random(Agent):
+    def __init__(self):
+        super().__init__()
+
+    def play(game: Game) -> tuple:
+        valid_moves = game.get_valid_moves()
+        move = sample(sorted(valid_moves), 1)[0]
+        return move
