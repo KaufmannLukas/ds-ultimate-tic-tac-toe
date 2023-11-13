0a6d1d5da92c9798fd33f44fcb90b7fba2bec25b
Nils Schillmann <nils.schillmann@pexon-consulting.de>
2023-11-13 17:21:19 +0100

    add print statements t lng_term_memory.py

--- long_term_memory.py
+++ long_term_memory.py
@@ -14,12 +14,15 @@ if train == True:
     mcts.train(num_iterations=1000000000, max_time=30)
 
 else:
+    print("load mcts memory")
     with open("data/mcts_ltmm.pkl", 'rb') as file:
         memory = pickle.load(file)
     game = Game()
+    print("build mcts agent")
     mcts = MCTS(memory=memory)
 
-    move = mcts.play(game=game)
+    print("make first move")
+    move = mcts.play(game=game, num_iterations=10, disable_progress_bar=False)
 
     print(move)
 
