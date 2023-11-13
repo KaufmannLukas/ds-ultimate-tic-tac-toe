61b264624bf545467655a854b5c7c1ba120be35c
Nils Schillmann <nils.schillmann@pexon-consulting.de>
2023-11-13 17:20:32 +0100

    loading memory to mcts_human

--- mcts_human.py
+++ mcts_human.py
@@ -1,11 +1,18 @@
+import pickle
 
 from environments.game import Game
 from agents.mcts import MCTS
 from agents.human import Human
 
+
 if __name__ == "__main__":
     game = Game()
-    computer_agent = MCTS()
+
+    print("load mcts memory")
+    with open("data/mcts_ltmm-10000000.pkl", 'rb') as file:
+        memory = pickle.load(file)
+
+    computer_agent = MCTS(memory=memory)
     human_agent = Human()
 
     counter = 0
