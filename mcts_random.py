3baff75d7284997229d1b8874e8cecf0f4c7f620
Nils Schillmann <nils.schillmann@pexon-consulting.de>
2023-11-14 16:32:28 +0100

    comment mcts_random.py

--- mcts_random.py
+++ mcts_random.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     logger.info("Main started...")
 
     num_iterations_list = [
-        #10,
+        # 10,
         20,
         # 50,
         # 100,
@@ -46,12 +46,14 @@ if __name__ == "__main__":
     for num_iterations in num_iterations_list:
         print(f"num_iterations: {num_iterations}")
         for i in tqdm(range(num_of_games)):
-            mcts_agent = MCTS(memory_path="data/mcts_fluid_ltmm.pkl", update_memory=True)
+            mcts_agent = MCTS(
+                memory_path="data/mcts_fluid_ltmm.pkl", update_memory=True)
 
             logger.info("Start new Game")
             game = Game()
             counter = 0
 
+            # check which player's turn it is
             while not game.done:
                 if (counter+i) % 2 == 0:
                     next_move = mcts_agent.play(
@@ -65,18 +67,20 @@ if __name__ == "__main__":
             logger.info(f"leave count: {count_leaves(mcts_agent.memory)}")
             mcts_agent.save_memory()
             logger.info("Game done")
-            if i % 2 == 0:
-                # player_x = mcts_agent
-                # player_o = random_agent
-                winner_table.append(
-                    [i, num_iterations, game.white.color, game.winner])
-            else:
-                winner_table.append(
-                    [i, num_iterations, game.black.color, game.winner])
+
+            '''save results to csv-file with winner-information / statistics for model evaluation'''
+            # if i % 2 == 0:
+            #     # player_x = mcts_agent
+            #     # player_o = random_agent
+            #     winner_table.append(
+            #         [i, num_iterations, game.white.color, game.winner])
+            # else:
+            #     winner_table.append(
+            #         [i, num_iterations, game.black.color, game.winner])
 
         # winner_dataframe = pd.DataFrame(
         #     winner_table, columns=["game_nr", "num_iter", "mcts_color", "winner"])
         # winner_dataframe.to_csv(f"data/random_vs_mcts_{num_iterations}_memory_1.csv")
         # winner_table = []
 
-        # logger.info("mcts main stopped")
+        logger.info("mcts vs. random main stopped")
