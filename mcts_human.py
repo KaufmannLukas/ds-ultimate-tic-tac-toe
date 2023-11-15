4f12fdb66a44cf67dfb730816c8e5900f63b88d8
Lukas Kaufmann <lks.kaufmann@gmx.de>
2023-11-15 14:28:20 +0100

    play human vs. mcts interface

--- mcts_human.py
+++ mcts_human.py
@@ -9,23 +9,26 @@ import pickle
 if __name__ == "__main__":
     game = Game()
     #implement the next two lines for using a memory_prone agent (like mcts_agent_01)
-    with open("data/mcts_ltmm.pkl", mode="rb") as file:
-        memory = pickle.load(file)
+    # with open("data/mcts_ltmm_02.pkl", mode="rb") as file:
+    #     memory = pickle.load(file)
     #change the agent to play against here:
-    computer_agent = MCTS(memory = memory)
+    computer_agent = MCTS(memory_path = "data/mcts_ltmm_02.pkl")
     human_agent = Human()
 
+    # TODO: improve interface (print local/global wins, etc.)
+    # TODO: implement game loop (replay)
     counter = 0
     while not game.done:
         print("-"*31)
         print(game)
         print("-"*31)
-        if counter % 2 == 0:
+        if counter % 2 != 0:
             next_move = human_agent.play(game)
         else:
             next_move = computer_agent.play(game, num_iterations=1000)
         print(f"last move: ({next_move[0]+1}, {next_move[1]+1})")
-        game.play(*next_move)
+        result = game.play(*next_move)
+        print(result)
         counter += 1
 
     print(game)
