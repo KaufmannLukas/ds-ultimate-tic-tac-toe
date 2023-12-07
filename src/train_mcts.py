da59b0b9d4a2fe1a53a47a39e24c21a4f5627dc7
Lukas Kaufmann <lks.kaufmann@gmx.de>
2023-12-07 18:59:30 +0100

    update .gitkeep local folder

--- src/train_mcts.py
+++ src/train_mcts.py
@@ -1,7 +1,13 @@
 from agents.mcts import MCTS
 
+'''
+Creates memory file for MCTS.
+Updates memory when re-running.
+NOTE: needs a LOT of iterations to achieve good perfomance, due to tree size,
+and therefore file size gets large quickly...
+'''
 
 if __name__ == "__main__":
-    mcts = MCTS(memory_path="data/models/mcts/test.pkl")
-    mcts.train(num_iterations=1000, max_time=100)
+    mcts = MCTS(memory_path="data/models/mcts/mcts_memory_small.pkl")
+    mcts.train(num_iterations=10_000, max_time=100)
 
